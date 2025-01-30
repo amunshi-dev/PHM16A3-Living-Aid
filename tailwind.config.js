a863ba38b897e31e1cdd3fa0266931280c9017d2
Alauddin Munshi <amunshi.dev@gmail.com>
2025-01-31 01:26:10 +0400

    Resolve setup issue and Add header

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    "./index.html",
+    "*", // Update this based on your file locations
+  ],
+  theme: {
+    extend: {
+      // colors: {
+      //   // Replace with your color code
+      //   clrBlack: "#111111",
+      //   clrAccent: "#ffdc26",
+      //   clrPrimary: "#fff",
+      //   clrSecondary: "#fff9f4",
+      //   clrPlaceholder: "#c4c4c4",
+      //   clrCardOrange: "#f9df8a",
+      // },
+    },
+  },
+  plugins: [],
+};
